4d3fcc23b9d962955280ff6adcedf81b20ba68e1
Aditya Makhija <354139685@educ.dpcdsb.org>
2025-07-04 18:26:57 -0400

    Initial Commit

---
+++ BeenThere/BeenThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeenThereApp.swift
+//  BeenThere
+//
+//  Created by Aditya Makhija on 2025-07-04.
+//
+
+import SwiftUI
+
+@main
+struct BeenThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeenThere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeenThere
+//
+//  Created by Aditya Makhija on 2025-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
